4f74cf56c72ecdf4d9cc61d7022bb519a833de95
WillLillis <wlillis@umass.edu>
2023-09-13 22:30:27 -0400

    Added skip functionality

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@
 use bitvec::prelude::*;
 use std::{env, fs, path::PathBuf, process};
 
-fn get_files(mut args: impl Iterator<Item = String>) -> Result<(PathBuf, PathBuf), &'static str> {
+fn get_files(
+    mut args: impl Iterator<Item = String>,
+) -> Result<(PathBuf, PathBuf, usize), &'static str> {
     args.next(); // ignore the program name
 
     let file_1 = match args.next() {
@@ -33,28 +35,49 @@ fn get_files(mut args: impl Iterator<Item = String>) -> Result<(PathBuf, PathBuf
         return Err("Invalid argument passed for file_2");
     }
 
-    return Ok((file_1, file_2));
+    let n_skip = match args.next() {
+        Some(arg) => arg.parse::<usize>().unwrap_or_else(|_| {
+            eprintln!("Invalid skip value passed, defaulting to 0");
+            0
+        }),
+        None => 0,
+    };
+
+    return Ok((file_1, file_2, n_skip));
 }
 
-fn cmp_files(buff_1: &Vec<u8>, buff_2: &Vec<u8>) -> usize {
-    let n_diffs = buff_1.iter().zip(buff_2.iter()).fold(0, |accum, (a, b)| {
-        let bits_1 = a.view_bits::<Msb0>();
-        let bits_2 = b.view_bits::<Msb0>();
+fn cmp_files(buff_1: &Vec<u8>, buff_2: &Vec<u8>, n_skip: usize) -> usize {
+    if n_skip >= buff_1.len() || n_skip >= buff_2.len() {
+        eprintln!("Warning! 'n_skip' value greater than one or both file lengths");
+    }
 
-        accum
-            + bits_1
-                .iter()
-                .zip(bits_2.iter())
-                .fold(0, |mini_accum, (bit_a, bit_b)| {
-                    mini_accum + if bit_a == bit_b { 0 } else { 1 }
-                })
-    });
+    let n_diffs = buff_1
+        .iter()
+        .skip(n_skip)
+        .zip(buff_2.iter().skip(n_skip))
+        .fold(0, |accum, (a, b)| {
+            let bits_1 = a.view_bits::<Msb0>();
+            let bits_2 = b.view_bits::<Msb0>();
+
+            accum
+                + bits_1
+                    .iter()
+                    .zip(bits_2.iter())
+                    .fold(0, |mini_accum, (bit_a, bit_b)| {
+                        mini_accum
+                            + if bit_a == bit_b {
+                                0
+                            } else {
+                                1
+                            }
+                    })
+        });
 
     return n_diffs + (buff_1.len().abs_diff(buff_2.len()) * u8::BITS as usize);
 }
 
 fn main() {
-    let (file_1, file_2) = get_files(env::args()).unwrap_or_else(|err| {
+    let (file_1, file_2, n_skip) = get_files(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
@@ -69,9 +92,15 @@ fn main() {
         process::exit(1);
     });
 
-    let diff = cmp_files(&buff_1, &buff_2);
+    let diff = cmp_files(&buff_1, &buff_2, n_skip);
 
     println!("Bitdiff: {diff}");
-    println!("Bitdiff / len(file_1): {}", diff as f64 / (buff_1.len() * u8::BITS as usize) as f64);
-    println!("Bitdiff / len(file_2): {}", diff as f64 / (buff_2.len() * u8::BITS as usize) as f64);
+    println!(
+        "Bitdiff / len(file_1): {}",
+        diff as f64 / (buff_1.len() * u8::BITS as usize) as f64
+    );
+    println!(
+        "Bitdiff / len(file_2): {}",
+        diff as f64 / (buff_2.len() * u8::BITS as usize) as f64
+    );
 }
